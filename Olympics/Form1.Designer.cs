799f7d15e22a3f8f0ebe2418f889a381eeb6eff2
Miklós Márton Szalay <miklos.szalay2@stud.uni-corvinus.hu>
2021-11-29 18:48:47 +0100

    kesz

--- Olympics/Form1.Designer.cs
+++ Olympics/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cbxEv = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExcel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbxEv
+            // 
+            this.cbxEv.FormattingEnabled = true;
+            this.cbxEv.Location = new System.Drawing.Point(163, 12);
+            this.cbxEv.Name = "cbxEv";
+            this.cbxEv.Size = new System.Drawing.Size(121, 24);
+            this.cbxEv.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Év";
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Location = new System.Drawing.Point(163, 70);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(121, 36);
+            this.btnExcel.TabIndex = 2;
+            this.btnExcel.Text = "Excelbe ment";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            this.btnExcel.Click += new System.EventHandler(this.btnExcel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExcel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxEv);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbxEv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExcel;
     }
 }
 
--- Olympics/Form1.cs
+++ Olympics/Form1.cs
@@ -1,20 +1,134 @@
-﻿using System;
+﻿using Olympics.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Olympics
 {
     public partial class Form1 : Form
     {
+        List<OlympicResult> results = new List<OlympicResult>();
         public Form1()
         {
             InitializeComponent();
+            Betolt("Summer_olympic_Medals.scv");
+            ComboFeltolt();
+            Osztalyozas();
+        }
+
+        private void Osztalyozas()
+        {
+            foreach (OlympicResult item in results)
+            {
+                item.Position = Helyezes(item);
+            }
+        }
+
+        int Helyezes(OlympicResult res)
+        {
+            int counter = 0;
+            var szurt = from x in results where x.Year == res.Year && x.Country != res.Country select x;
+            foreach (OlympicResult item in szurt)
+            {
+                if (item.Medals[0] > res.Medals[0]) counter++;
+                else if ((item.Medals[0] == res.Medals[0]) && (item.Medals[1] > res.Medals[1])) counter++;
+                else if ((item.Medals[0] == res.Medals[0]) && (item.Medals[1] == res.Medals[1]) && (item.Medals[2] > res.Medals[2])) counter++;
+            }
+            return counter + 1;
+        }
+
+
+        private void ComboFeltolt()
+        {
+            var years = (from x in results orderby x.Year select x.Year).Distinct();
+            cbxEv.DataSource = years.ToList();
+
+        }
+
+        void Betolt(string fajlnev)
+        {
+            using (StreamReader sr = new StreamReader(fajlnev))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string sor = sr.ReadLine();
+                    string[] mezok = sor.Split(',');
+                    OlympicResult or = new OlympicResult();
+                    or.Year = int.Parse(mezok[0]);
+                    or.Country = mezok[3];
+                    int[] mtomb = new int[3];
+                    mtomb[0] = int.Parse(mezok[5]);
+                    mtomb[1] = int.Parse(mezok[6]);
+                    mtomb[2] = int.Parse(mezok[7]);
+                    or.Medals = mtomb;
+                    results.Add(or);
+                }
+            }
+        }
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+        private void btnExcel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                ExcelFeltolt();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        private void ExcelFeltolt()
+        {
+            var headers = new string[]
+            {
+                "Helyezés",
+                "Ország",
+                "Arany",
+                "Ezüst",
+                "Bronz"
+
+            };
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+
+            }
+            var filteredResults = from x in results where x.Year == (int)cbxEv.SelectedItem orderby x.Position select x;
+            int aktsor = 2;
+            foreach (var item in filteredResults)
+            {
+                xlSheet.Cells[aktsor, 1] = item.Position;
+                xlSheet.Cells[aktsor, 2] = item.Country;
+                for (int i = 0; i < 3; i++)
+                {
+                    xlSheet.Cells[aktsor, 3 + i] = item.Medals[i];
+                }
+                aktsor++;
+            }
         }
     }
 }
