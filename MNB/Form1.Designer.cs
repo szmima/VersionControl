381dceb42af2356ee91b192f8e3e1d873e7cb8e7
Miklós Márton Szalay <miklos.szalay2@stud.uni-corvinus.hu>
2021-12-10 00:45:53 +0100

    kesz

--- MNB/Form1.Designer.cs
+++ MNB/Form1.Designer.cs
@@ -28,13 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chartRateData = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tolPicker = new System.Windows.Forms.DateTimePicker();
+            this.igPicker = new System.Windows.Forms.DateTimePicker();
+            this.cbValuta = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRateData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(318, 310);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // chartRateData
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chartRateData.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chartRateData.Legends.Add(legend4);
+            this.chartRateData.Location = new System.Drawing.Point(400, 25);
+            this.chartRateData.Name = "chartRateData";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chartRateData.Series.Add(series4);
+            this.chartRateData.Size = new System.Drawing.Size(532, 300);
+            this.chartRateData.TabIndex = 1;
+            this.chartRateData.Text = "chart1";
+            // 
+            // tolPicker
+            // 
+            this.tolPicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.tolPicker.Location = new System.Drawing.Point(12, 371);
+            this.tolPicker.Name = "tolPicker";
+            this.tolPicker.Size = new System.Drawing.Size(200, 22);
+            this.tolPicker.TabIndex = 2;
+            this.tolPicker.ValueChanged += new System.EventHandler(this.paramChanged);
+            // 
+            // igPicker
+            // 
+            this.igPicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.igPicker.Location = new System.Drawing.Point(237, 371);
+            this.igPicker.Name = "igPicker";
+            this.igPicker.Size = new System.Drawing.Size(200, 22);
+            this.igPicker.TabIndex = 3;
+            this.igPicker.ValueChanged += new System.EventHandler(this.paramChanged);
+            // 
+            // cbValuta
+            // 
+            this.cbValuta.FormattingEnabled = true;
+            this.cbValuta.Items.AddRange(new object[] {
+            "EUR",
+            "USD",
+            "GBP"});
+            this.cbValuta.Location = new System.Drawing.Point(462, 369);
+            this.cbValuta.Name = "cbValuta";
+            this.cbValuta.Size = new System.Drawing.Size(121, 24);
+            this.cbValuta.TabIndex = 4;
+            this.cbValuta.SelectedIndexChanged += new System.EventHandler(this.paramChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1085, 450);
+            this.Controls.Add(this.cbValuta);
+            this.Controls.Add(this.igPicker);
+            this.Controls.Add(this.tolPicker);
+            this.Controls.Add(this.chartRateData);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRateData)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartRateData;
+        private System.Windows.Forms.DateTimePicker tolPicker;
+        private System.Windows.Forms.DateTimePicker igPicker;
+        private System.Windows.Forms.ComboBox cbValuta;
     }
 }
 
--- MNB/Form1.cs
+++ MNB/Form1.cs
@@ -1,20 +1,120 @@
-﻿using System;
+﻿using MNB.Entities;
+using MNB.MNBServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
 
 namespace MNB
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> _rates = new BindingList<RateData>();
+        BindingList<string> currencies = new BindingList<string>();
         public Form1()
         {
             InitializeComponent();
+            loadCurrencyxml(GetCurrencies);
+            cbValuta.DataSource = currencies;
+            RefreshData();
+        }
+
+        private void RefreshData()
+        {
+            if (cbValuta.SelectedItem == null) return;
+            _rates.Clear();
+            LoadXML(GetRates());
+
+            dataGridView1.DataSource = _rates;
+
+            MakeChart();
+        }
+
+        private void MakeChart()
+        {
+            chartRateData.DataSource = _rates;
+            Series sorozatok = chartRateData.Series[0];
+            sorozatok.ChartType = SeriesChartType.Line;
+            sorozatok.XValueMember = "Data";
+            sorozatok.YValueMembers = "Value";
+
+            var jelmagyarazat = chartRateData.Legends[0];
+            jelmagyarazat.Enabled = false;
+
+            var diagramterulet = chartRateData.ChartAreas[0];
+            diagramterulet.AxisY.IsStartedFromZero = false;
+            diagramterulet.AxisY.MajorGrid.Enabled = false;
+            diagramterulet.AxisX.MajorGrid.Enabled = false;
+                
+
+        }
+
+        private void LoadXML(string xmlstring )
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(xmlstring);
+            foreach (XmlElement item in xml.DocumentElement)
+            {
+                RateData r = new RateData();
+                r.Date = DateTime.Parse(item.GetAttribute("date"));
+                var ChildElement = (XmlElement)item.ChildNodes[0];
+                if (ChildElement != null)
+                {
+                    r.Currency = ChildElement.GetAttribute("curr");
+                    decimal unit = decimal.Parse(ChildElement.GetAttribute("unit"));
+                    r.Value = decimal.Parse(ChildElement.InnerText);
+                    if (unit != 0)
+                        r.Value = r.Value / unit;
+                    _rates.Add(r);
+                }
+
+                
+            }
+        }
+        private void loadCurrencyxml(string xmlstring)
+        {
+            currencies.Clear();
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(xmlstring);
+            foreach (XmlElement item in xml.DocumentElement.ChildNodes[0])
+            {
+                string s = item.InnerText;
+                currencies.Add(s);
+            }
+        }
+
+        private string GetRates()
+        {
+           
+            var MNBServiceReference = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody req = new GetExchangeRatesRequestBody();
+            req.currencyNames = cbValuta.SelectedItem.ToString();
+            req.startDate = tolPicker.Value.ToString("yyyy-MM-dd");
+            req.endDate = igPicker.Value.ToString("yyyy-MM-dd");
+            var response = MNBServiceReference.GetExchangeRates(req);
+            return response.GetExchangeRatesResult;
+
+        }
+        private string GetCurrencies()
+        {
+            var MNBServiceReference = new MNBArfolyamServiceSoapClient();
+            GetCurrenciesRequestBody req = new GetCurrenciesRequestBody();
+            var resp = MNBServiceReference.GetCurrencies(req);
+            string result = resp.GetCurrenciesResult;
+            File.WriteAllText("currency.xml",result);
+            return resp.GetCurrenciesResult;
+        }
+        private void paramChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
